e2325908287de7f0bfb4024f7bd0f73d32ea3e2b
釜野由里佳 <yrkmn19@gmail.com>
2018-02-10 12:38:52 +0900

    初めてみたエラー

--- MenterBook2/ViewController.swift
+++ MenterBook2/ViewController.swift
@@ -9,15 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var mentorArray: [Mentor] = []
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabl: UILabel!
+    
+    var index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mentorArray.append(Mentor(name: "ながた", imageName: "nagata.jpg", course: "iPhone"))
+        mentorArray.append(Mentor(name: "りょう", imageName: "ryo.jpg", course: "Unity"))
+        mentorArray.append(Mentor(name:"たいてぃ", imageName: "taithi.jpg", course: "WebS,WebD"))
+        
+        setUI()
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func setUI() {
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabl.text = mentorArray[index].course
+    }
+    @IBAction func mae(){
+        index =  index  - 1
+        setUI()
+    }
+    
+    @IBAction func tugi(){
+        index = index  + 1
+        setUI()
     }
 
 
